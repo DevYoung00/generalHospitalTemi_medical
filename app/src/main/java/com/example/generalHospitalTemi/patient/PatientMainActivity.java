405500bb1c4080c88f74980288b009525faaae59
DevYoung00 <dkdud203@naver.com>
2023-11-29 19:42:03 +0900

    Feat: firebase에서 값 가져오는 예시 코드 추가

--- app/src/main/java/com/example/generalHospitalTemi/patient/PatientMainActivity.java
+++ app/src/main/java/com/example/generalHospitalTemi/patient/PatientMainActivity.java
@@ -1,20 +1,41 @@
 package com.example.generalHospitalTemi.patient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.generalHospitalTemi.R;
 import com.example.generalHospitalTemi.databinding.ActivityMedicalMainBinding;
 import com.example.generalHospitalTemi.databinding.ActivityPatientMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
 
 public class PatientMainActivity extends AppCompatActivity {
 
     private ActivityPatientMainBinding binding;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPatientMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        databaseReference.child("rfid").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object rfidValue = snapshot.getValue();
+                if (rfidValue != null) {
+                    binding.firebaseText.setText("RFID: " + rfidValue.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("tag", "Failed to read RFID value.", error.toException());
+            }
+        });
     }
 }
