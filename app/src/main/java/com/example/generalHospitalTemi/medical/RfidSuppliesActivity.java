adfde2439fbbdb61425247d5569fe89c0784aa11
DevYoung00 <dkdud203@naver.com>
2023-11-29 18:41:00 +0900

    Feat: medical, patient main xml 구현

---
+++ app/src/main/java/com/example/generalHospitalTemi/medical/RfidSuppliesActivity.java
@@ -0,0 +1,16 @@
+package com.example.generalHospitalTemi.medical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.generalHospitalTemi.R;
+
+public class RfidSuppliesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rfid_supplies);
+    }
+}
