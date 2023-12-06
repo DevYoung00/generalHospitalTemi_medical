fb06ad63d3f086474a6f81a3bb6f94225ebcd526
DevYoung00 <dkdud203@naver.com>
2023-12-06 19:43:51 +0900

    Feat: 문진표 작성 xml 퍼블리싱

---
+++ app/src/main/java/com/example/generalHospitalTemi/patient/register/RegisterActivity2.java
@@ -0,0 +1,16 @@
+package com.example.generalHospitalTemi.patient.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.generalHospitalTemi.R;
+
+public class RegisterActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
+}
