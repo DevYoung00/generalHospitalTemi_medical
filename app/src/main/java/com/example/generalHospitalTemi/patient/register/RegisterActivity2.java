a17713730aa0a5ec4daee35c1fcd8952864f84b1
bakukun <h1101201@gmail.com>
2023-12-20 16:28:24 +0900

    수정

--- app/src/main/java/com/example/generalHospitalTemi/patient/register/RegisterActivity2.java
+++ app/src/main/java/com/example/generalHospitalTemi/patient/register/RegisterActivity2.java
@@ -1,4 +1,4 @@
-/* package com.example.generalHospitalTemi.patient.register;
+package com.example.generalHospitalTemi.patient.register;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,9 +26,8 @@ public class RegisterActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPatientRegister2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        databaseReference = FirebaseDatabase.getInstance().getReference("cardnum");
-        checkCardnum();
-        binding.register2PrevText.setOnClickListener(new View.OnClickListener() {
+
+        binding.register2MainText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(RegisterActivity2.this, PatientMainActivity.class);
@@ -38,35 +37,6 @@ public class RegisterActivity2 extends AppCompatActivity {
 
 
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // 액티비티가 재시작될 때 checkCardnum() 메소드를 호출합니다.
-        checkCardnum();
-    }
 
-    private void checkCardnum() {
-        databaseReference.orderByKey().limitToLast(1).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    DataSnapshot lastNode = snapshot.getChildren().iterator().next();
-                    Object cardnum_before = lastNode.getValue();
-                    String cardnum = cardnum_before.toString();
-                    Log.d("Cardnum Log", "Cardnum: " + cardnum);
-                    if ("99101208166".equals(cardnum)) {
-                        startActivity(new Intent(RegisterActivity1.this, RegisterYesActivity.class));
-                    } else {
-                        startActivity(new Intent(RegisterActivity1.this, RegisterNoActivity.class));
-                    }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // 에러 처리
-            }
-        });
-    }
 }
-*/
